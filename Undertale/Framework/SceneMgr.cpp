ee00203fc6630fe5f043a78b73f60ab58b8dd7c3
이재문 <990914s@gmail.com>
2025-07-22 11:50:49 +0900

    초기커밋

---
+++ Undertale/Framework/SceneMgr.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "SceneMgr.h"
+#include "SceneDev1.h"
+//#include "SceneDev2.h"
+
+
+void SceneMgr::Init()
+{
+	scenes.insert({ SceneIds::Dev1 ,new SceneDev1() });
+	//scenes.insert({ SceneIds::Title ,new SceneDev2() });
+	
+
+	for (auto scene : scenes)
+	{
+		scene.second->Init();
+	}
+
+	currentScene = startScene;
+	scenes[currentScene]->Enter();
+}
+
+void SceneMgr::Release()
+{
+	for (auto scene : scenes)
+	{
+		if (scene.second->Id == currentScene)
+		{
+			scene.second->Exit();
+		}
+		scene.second->Release();
+		delete scene.second;
+	}
+	scenes.clear();
+}
+
+void SceneMgr::ChangeScene(SceneIds id)
+{
+	nextScene = id;
+}
+
+void SceneMgr::Update(float dt)
+{
+	if (nextScene != SceneIds::None)
+	{
+		scenes[currentScene]->Exit();
+		currentScene = nextScene;
+		nextScene = SceneIds::None;
+		scenes[currentScene]->Enter();
+	}
+
+	scenes[currentScene]->Update(dt);
+}
+
+void SceneMgr::Draw(sf::RenderWindow& window)
+{
+	scenes[currentScene]->Draw(window);
+}
