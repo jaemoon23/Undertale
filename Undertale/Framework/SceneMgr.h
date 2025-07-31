13a37428b559a8d4278d95d9ac098d8b8714ada8
이재문 <990914s@gmail.com>
2025-07-31 18:06:36 +0900

    머지후

--- Undertale/Framework/SceneMgr.h
+++ Undertale/Framework/SceneMgr.h
@@ -9,7 +9,7 @@ protected:
 
 	std::unordered_map<SceneIds, Scene*> scenes;
 
-	SceneIds startScene = SceneIds::TemMieShop;
+	SceneIds startScene = SceneIds::MapSans;
 	SceneIds currentScene = SceneIds::None;
 	SceneIds nextScene = SceneIds::None;
 
